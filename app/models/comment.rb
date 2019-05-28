5dde0776e3e9153b75b2101072e52ef94108662a
christophe gillis <chris.tim.gillis@gmail.com>
2019-05-28 12:34:47 +0200

    added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :todo
+
+  validates_presence_of :user, :todo
+  validates :content, presence: { message: "can't be blank" }
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,8 @@
 class Todo < ApplicationRecord
   has_many :comments
   belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: { message: "don't forget your title" }
+  validates :task, presence: { message: "please provide a task" }
 end
