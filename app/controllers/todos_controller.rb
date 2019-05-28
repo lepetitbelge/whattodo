7d8b955414f42b7c3d4fc16d7aa730ff76668049
christophe gillis <chris.tim.gillis@gmail.com>
2019-05-28 13:01:22 +0200

    added todo controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,32 @@
+class TodosController < ApplicationController
+  before_action :find_todo, only: %i[update destroy]
+  # add pundit rule
+
+  def index
+    @todos = Todo.all
+  end
+
+  def create
+    Todo.create(todo_params)
+    redirect_to root_path
+  end
+
+  def update
+    redirect_to root_path
+  end
+
+  def destroy
+    todo.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:description, :priority)
+  end
+end
