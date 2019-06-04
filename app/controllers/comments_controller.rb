f6d82348433f8e4f0985573f0ea2a11f234b18d2
christophe gillis <chris.tim.gillis@gmail.com>
2019-06-04 15:13:43 +0200

    allowed user to create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :find_comment, only: %i[destroy]
+
   def index
   end
 
   def create
+    @comment = Comment.create(comment_params)
+    authorize @comment
+    redirect_to root_path
   end
 
-  def delete
+  def destroy
+    @comment.destroy
+    authorize @comment
+    redirect_to root_path
+  end
+
+  private
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :todo_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: 'todos#index'
 
   resources :todos
+  resources :comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
